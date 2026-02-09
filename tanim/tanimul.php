6ac441c939cc1fad315b7a3c7d138d4faa3f274e
Tanimul2004 <tanimul2004@gmail.com>
2026-02-10 01:19:27 +0600

    tanim has changed the file

---
+++ tanim/tanimul.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1>Welcome to My Wesdfdfd sdf bsite</h1>
+    <p>This is a sample paragraph to demonstrate the HTML structure.</p>
+    <?php
+    // This is a single-line comment
+    echo "Hello, World!"; // This is a single-line comment
+    ?>
+    <h1><?php echo "Hello, World!"; ?></h1>
+</body>
+</html>
+
+    <!-- <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            font-size: 16px;
+            color: #666;
+        }
+    </style> -->
